3f962a98ad7de3fe550207d1594356f5fccca85d
Krystle Mitchell <mitchell.krystle2@gmail.com>
2021-11-12 09:59:47 -0800

    created states

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
+
+const intialPizza ={
+    name: string,
+    size: string,
+    topping1: bool,
+    topping2: bool,
+    special: string,
+}
+const [toppings, setToppings] = useState([""])
+
+  
+}
 
 const App = () => {
+  const [pizza, setPizza] = useState [""]
+  const [toppings, setToppings] = useState([""])
+
   return (
     <>
       <h1>Lambda Eats</h1>
