6d3e0ee834b35eb3e78aa066e49502abb48a5d1a
Krystle Mitchell <mitchell.krystle2@gmail.com>
2021-11-12 14:35:50 -0800

    finished lambda pizza form

--- src/App.js
+++ src/App.js
@@ -1,34 +1,82 @@
 import React, {useState} from "react";
-import Form from './components/Form.js'
+import Form from './components/Form.js';
+import { Route , Link } from 'react-router-dom';
+import  Pizza  from "./components/Pizza.js";
 
+const initialPizza = {
+  name: " ",
+  type: " ",
+  size: " ",
+  toppings:[]
+  // special:"2 for 1"
+  // price:"$5"
 
-
-
-
-
-
-  
+}
 
 
 const App = () => {
-  const [pizza, setPizza] = useState(intialPizza)
-  // const [toppings, setToppings] = useState([""])
+  const [name, setName] = useState("")
+  const [pizza, setPizza] = useState();
+  const [sauce, setSauce] = useState([]);
+   const [toppings, setToppings] = useState([]);
+   const [size, setSize] = useState("");
+   const [order, setOrder] =useState([]);
+   
 
+ 
+  
+  const handleSauce = (evt) => {
+    setSauce(evt.target.value);
+  };
+  
+  const handleToppings = (evt) => {
+    setToppings(evt.target.value);
+  };
+  
+  const handleSize = (evt) => {
+    setSize(evt.target.value);
+    console.log(evt.target.value)
+  };
 
-
+  const handleOrder = () => {
+    setOrder([...order, {name:name}])
+  }
+  
   return (
-    <div className="pizzaApp">
-        <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-      
-      <Form /> 
+    <div className="App">
+      <header>
 
+      
+        <h1>Lambda Eats</h1>
+        <p>It's Pizza Time!!</p>
+        {/* <nav>
+          <Link to="/">Home</Link>
+          <Link to="/form">Choose Your Pizza</Link>
+          <Link to="/pizza">Order is Placed</Link>
+          </nav>
+          <Route path="/pizza">
+         
+
+            <Pizza pizza={pizza} />
+            </Route> */}
+
+            <Form 
+            handleDrop= {handleSize}
+             handleSauce ={ handleSauce}
+             handleToppings= {handleToppings}
+             handleOrder= {handleOrder}
+            
+            />
+      
+     
+
+
+      </header>
+
+        
       </div>
      
-  )    
-
- 
- 
+  );    
      
 }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,229 +1,98 @@
-import React from 'react';
+import React from "react";
 
 
-export default function Form () {
-return (
+export default function Form(props) {
+  const { handleSize, handleSauce, handleToppings, handleOrder } = props;
+  console.log(props);
+  return (
+    <div className="formInfo">
+      {/* DropDown Menu */}
+      <label>
+        Choice of size
+        <select name="size" onChange={handleSize}>
+          <option value="">--- Select ---</option>
+          <option value="12-inch"> 12-inch </option>
+          <option value="8-inch"> 8-Inch </option>
+          <option value="4-inch"> 4-Inch</option>
+        </select>
+      </label>
 
+      {/* Radio Buttons */}
+    <div className="sauces">
+      <label>
+        Choice of Sauce
+        <p> Required</p>
 
-<div className="formInfo">
+        </label> Spinach Alfredo
+        <label>
+        <input type="radio" id="" name="sauce" onChange={handleSauce} />
+      </label>
 
-{/* DropDown Menu */}
-<label> 
-    Choice of size
-    
-    <select value={values.size} name="size" onChange={onChange}>
-        <option value="">--- Select ---</option>
-        <option value="12-inch"> 6-inch </option>
-        <option value="8-inch"> 8-Inch </option>
-        <option value="4-inch"> 4-Inch</option>
+      <label> Original Red
+        <input type="radio" id="" name="sauce" onChange={handleSauce} />
+      </label>
+
+      <label> Garlic Ranch
+        <input type="radio" id="" name="sauce" onChange={handleSauce} />
+      </label>
+
+      <label> BBQ Sauce
+        <input type="radio" id="" name="sauce" onChange={handleSauce} />
+      </label>
+      </div>
+
+      {/* Checkboxes */}
+
+      <div className="toppings">
+
+      <label> Peppercorn
+        <input type="checkbox" name="pepperoni" onChange={handleToppings} />
+      </label>
+
+      <label> Sausage
+        <input type="checkbox" name="pepperoni" onChange={handleToppings} />
+      </label>
+
+      <label> Black Olives
+        <input type="checkbox" name="pepperoni" onChange={handleToppings} />
+      </label>
+
+      <label> Artichoke Hearts
+        <input type="checkbox" name="pepperoni" onChange={handleToppings} />
+      </label>
+
+      <label> Three Cheese
+        <input type="checkbox" name="pepperoni" onChange={handleToppings} />
+      </label>
+
+      </div>
+
+      {/* Toggle */}
+      <div className="choices">
         
-    </select>
+        Choice of Substitute:
+        <label class="swtich"> Gluten-Free
+        <input type="checkbox" />
+        <span class="slider"></span>
+      </label>
+      <label class="switch"> Vegetarian
+        <input type="checkbox" />
+        <span class="slider"></span>
+      </label>
+      </div>
 
+      {/* Notes */}
+      <div className="notes">
+      <label> 
+        <input type="text" name="name" placeholder="Additional Notes" />
+      </label>
+      </div>
 
-</label>
-
-{/* Radio Buttons */}
-
-
-<label> 
-    Choice of Sauce
-    <p> Required</p>
-    <input
-    type="radio"
-    id=""
-    name="sauce"
-    onChange={onChange}
-    />
-
-</label>
-
-    <label>
-    <input
-    type="radio"
-    id=""
-    name="sauce"
-    onChange={onChange}
-    />
-    </label>
-
-<label>
-    <input
-    type="radio"
-    id=""
-    name="sauce"
-    onChange={onChange}
-    />
-</label>
-
-<label>
-    <input
-    type="radio"
-    id=""
-    name="sauce"
-    onChange={onChange}
-    />
-</label>
-
-{/* Checkboxes */}
-
-<label>
-    <input
-    type="checkbox"
-    value="toppings"
-    name="pepperoni"
-    checked={values.pepperoni}
-    />
-</label>
-
-<label>
-    <input
-    type="checkbox"
-    value="toppings"
-    name="pepperoni"
-    checked={values.pepperoni}
-    />
-</label>
-
-<label>
-    <input
-    type="checkbox"
-    value="toppings"
-    name="pepperoni"
-    checked={values.pepperoni}
-    />
-</label>
-
-<label>
-    <input
-    type="checkbox"
-    value="toppings"
-    name="pepperoni"
-    checked={values.pepperoni}
-    />
-</label>
-
-<label>
-    <input
-    type="checkbox"
-    value="toppings"
-    name="pepperoni"
-    checked={values.pepperoni}
-    />
-</label>
-
-<label>
-    <input
-    type="checkbox"
-    value="toppings"
-    name="pepperoni"
-    checked={values.pepperoni}
-    />
-</label>
-
-<label>
-    <input
-    type="checkbox"
-    value="toppings"
-    name="pepperoni"
-    checked={values.pepperoni}
-    />
-</label>
-
-<label>
-    <input
-    type="checkbox"
-    value="toppings"
-    name="pepperoni"
-    checked={values.pepperoni}
-    />
-</label>
-
-<label>
-    <input
-    type="checkbox"
-    value="toppings"
-    name="pepperoni"
-    checked={values.pepperoni}
-    />
-</label>
-
-<label>
-    <input
-    type="checkbox"
-    value="toppings"
-    name="pepperoni"
-    checked={values.pepperoni}
-    />
-</label>
-
-<label>
-    <input
-    type="checkbox"
-    value="toppings"
-    name="pepperoni"
-    checked={values.pepperoni}
-    />
-</label>
-
-<label>
-    <input
-    type="checkbox"
-    value="toppings"
-    name="pepperoni"
-    checked={values.pepperoni}
-    />
-</label>
-
-<label>
-    <input
-    type="checkbox"
-    value="toppings"
-    name="pepperoni"
-    checked={values.pepperoni}
-    />
-</label>
-
-<label>
-    <input
-    type="checkbox"
-    value="toppings"
-    name="pepperoni"
-    checked={values.pepperoni}
-    />
-</label>
-
-{/* Toggle */}
-<label class ="swtich"> Choice of Substitute
-    <input 
-    type="checkbox"/>
-    <span class ="slider"></span>
-</label>
-<label>
-<input 
-    type="checkbox"/>
-    <span class ="slider"></span>
-</label>
-
-{/* Notes */}
-<label>
-    <input
-    type="text"
-    name="name"
-    value={values.name}
-    placeholder="Special Notes"
-    />
-</label>
-
-{/* Add to Cart Button */}
-
-<button onClick={}>Add to Order</button>
-
-
-
-
-
-
-
+      {/* Add to Cart Button */}
+<div className=" button">
+<button onClick={handleOrder}>Add to Order</button>
 </div>
-)
+    
+    </div>
+  );
 }
